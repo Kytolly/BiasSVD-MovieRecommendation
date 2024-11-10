1cb683615ee8f676f28db18739054f36b929bd31
unknown <1392875097@qq.com>
2024-11-10 09:21:56 +0800

    v0.2.1:delete dev in main

--- dev/env.py
+++
@@ -1,8 +0,0 @@
-
-class Env():
-    def __init__(self):
-        self.config_path = 'config/config.yaml'
-        self.movies_path = 'data/small/movies.csv'
-        self.ratings_path = 'data/small/ratings.csv'
-        self.log_path = 'log/info.log' 
-        self.model_dir = 'model/'
